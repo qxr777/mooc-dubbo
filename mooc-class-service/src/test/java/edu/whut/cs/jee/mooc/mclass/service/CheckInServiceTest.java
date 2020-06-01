37b94ff837a95266bbf9fa1e65452760dd858289
qxr777 <qixin_y20005@163.com>
2020-06-01 17:49:02 +0800

    修正出勤单元测试

--- mooc-class-service/src/test/java/edu/whut/cs/jee/mooc/mclass/service/CheckInServiceTest.java
+++ mooc-class-service/src/test/java/edu/whut/cs/jee/mooc/mclass/service/CheckInServiceTest.java
@@ -1,27 +1,27 @@
 package edu.whut.cs.jee.mooc.mclass.service;
 
+import edu.whut.cs.jee.mooc.common.constant.MoocClassConstatnts;
 import edu.whut.cs.jee.mooc.common.exception.APIException;
 import edu.whut.cs.jee.mooc.mclass.dto.AttendanceDto;
 import edu.whut.cs.jee.mooc.mclass.dto.CheckInDto;
 import edu.whut.cs.jee.mooc.mclass.dto.LessonDto;
-import edu.whut.cs.jee.mooc.mclass.service.impl.MoocClassServiceImpl;
+import edu.whut.cs.jee.mooc.mclass.model.Lesson;
+import edu.whut.cs.jee.mooc.mclass.repository.LessonRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
-import java.text.ParseException;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
+import java.util.List;
 
 @Slf4j
 @RunWith(SpringRunner.class)
@@ -46,14 +46,21 @@ public class CheckInServiceTest {
             .lessonId(lessonId)
             .build();
 
-    @Autowired
+    @Resource
     CheckInService checkInService;
 
-    @Autowired
-    MoocClassServiceImpl moocClassService;
+    @Resource
+    MoocClassService moocClassService;
+
+    @Resource
+    LessonRepository lessonRepository;
 
     @Before
     public void prepareTestObjects() {
+        List<Lesson> lessons = lessonRepository.findByMoocClassIdAndStatus(moocClassId, MoocClassConstatnts.LESSON_STATUS_SERVICING);
+        for(Lesson lesson : lessons) {
+            moocClassService.endLesson(lesson.getId());
+        }
         LessonDto lessonDto = moocClassService.startLesson(moocClassId);
         lessonId = lessonDto.getId();
         checkInDto.setLessonId(lessonId);
@@ -66,7 +73,7 @@ public class CheckInServiceTest {
     }
 
     @Test
-    public void testSaveAttendance() throws ParseException, APIException {
+    public void testSaveAttendance() throws APIException {
         AttendanceDto attendanceDto = AttendanceDto.builder()
                 .userId(studentId1)
                 .checkInId(checkInId)
@@ -81,7 +88,7 @@ public class CheckInServiceTest {
     }
 
     @Test
-    public void testCloseCheckIn() throws ParseException, APIException {
+    public void testCloseCheckIn() throws APIException {
         checkInService.closeCheckIn(checkInId);
     }
 
