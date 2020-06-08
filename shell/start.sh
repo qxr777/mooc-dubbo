fafa7ec1b846d4ba09f6aa5b0f6233ee406c5863
qxr777 <qixin_y20005@163.com>
2020-06-08 16:03:34 +0800

    分离dubbo属性文件

--- shell/start.sh
+++ shell/start.sh
@@ -1,1 +1,6 @@
-nohup java -jar mooc-class-2.1.2.RELEASE.jar --spring.profiles.active=prod &
+#nohup java -jar mooc-class-service-1.0-SNAPSHOT.jar --spring.dubbo.protocol.port=20883 &
+#nohup java -jar mooc-class-service-1.0-SNAPSHOT.jar --spring.dubbo.protocol.port=20882 &
+nohup java -jar mooc-class-service-1.0-SNAPSHOT.jar --spring.dubbo.protocol.port=20881 &
+
+#nohup java -jar mooc-class-web-1.0-SNAPSHOT.jar --spring.dubbo.protocol.port=10880 --server.port=8080 &
+#nohup java -jar mooc-class-web-1.0-SNAPSHOT.jar --spring.dubbo.protocol.port=10881 --server.port=8081 &
