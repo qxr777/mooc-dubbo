1c4601aa1b9f515a43aa4b64d05f803dedb497c4
qxr777 <qixin_y20005@163.com>
2021-06-23 16:36:55 +0800

    改进监控程序启动方式

--- mooc-class-service/src/main/java/edu/whut/cs/jee/mooc/MoocClassServiceApplication.java
+++ mooc-class-service/src/main/java/edu/whut/cs/jee/mooc/MoocClassServiceApplication.java
@@ -1,16 +1,26 @@
 package edu.whut.cs.jee.mooc;
 
+import cn.hutool.core.convert.Convert;
+import cn.hutool.core.thread.ThreadUtil;
+import cn.hutool.core.util.NumberUtil;
+import cn.hutool.core.util.StrUtil;
 import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import java.io.IOException;
+import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 @Slf4j
 @EnableTransactionManagement
@@ -22,9 +32,50 @@ public class MoocClassServiceApplication implements ApplicationRunner {
 
     public static void main(String[] args) throws IOException {
 
+        int port = 0;
+        int defaultDubboPort = 20881;
 
-        SpringApplication.run(MoocClassServiceApplication.class, args);
-//        System.in.read(); // press any key to exit
+        if(null!=args && 0!=args.length) {
+            for (String arg : args) {
+                if(arg.startsWith("port=")) {
+                    String strPort= StrUtil.subAfter(arg, "port=", true);
+                    if(NumberUtil.isNumber(strPort)) {
+                        port = Convert.toInt(strPort);
+                    }
+                }
+            }
+        }
+
+        if(0==port) {
+            Future<Integer> future = ThreadUtil.execAsync(() ->{
+                int p = 0;
+                System.out.printf("请于5秒钟内输入dubbo服务监听端口号, 推荐  %d ,超过5秒将默认使用 %d %n",defaultDubboPort,defaultDubboPort);
+                Scanner scanner = new Scanner(System.in);
+                while(true) {
+                    String strPort = scanner.nextLine();
+                    if(!NumberUtil.isInteger(strPort)) {
+                        System.err.println("只能是数字");
+                        continue;
+                    }
+                    else {
+                        p = Convert.toInt(strPort);
+                        scanner.close();
+                        break;
+                    }
+                }
+                return p;
+            });
+            try{
+                port=future.get(5, TimeUnit.SECONDS);
+            }
+            catch (InterruptedException | ExecutionException | TimeoutException e){
+                port = defaultDubboPort;
+            }
+        }
+        new SpringApplicationBuilder(MoocClassServiceApplication.class).properties("spring.dubbo.protocol.port=" + port).run(args);
+
+
+//        SpringApplication.run(MoocClassServiceApplication.class, args);
 
         args = new String[] { "spring" };
         com.alibaba.dubbo.container.Main.main(args);
--- mooc-class-web/src/main/java/edu/whut/cs/jee/mooc/MoocClassWebApplication.java
+++ mooc-class-web/src/main/java/edu/whut/cs/jee/mooc/MoocClassWebApplication.java
@@ -1,21 +1,53 @@
 package edu.whut.cs.jee.mooc;
 
+import cn.hutool.core.convert.Convert;
+import cn.hutool.core.thread.ThreadUtil;
+import cn.hutool.core.util.NumberUtil;
+import cn.hutool.core.util.StrUtil;
 import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
 @Slf4j
-//@EnableTransactionManagement
 @SpringBootApplication
 @EnableDubboConfiguration
 public class MoocClassWebApplication implements ApplicationRunner {
 
     public static void main(String[] args) {
-        SpringApplication.run(MoocClassWebApplication.class, args);
+        int dubboPort = 0;
+        int tomcatPort = 0;
+        int defaultDubboPort = 10881;
+        int defaultTomcatPort = 8080;
+
+
+        System.out.printf("请于5秒钟内输入dubbo服务监听端口号, 推荐  %d %n", defaultDubboPort);
+        Scanner scanner = new Scanner(System.in);
+        String strPort = scanner.nextLine();
+        dubboPort = Convert.toInt(strPort);
+
+        System.out.printf("请于5秒钟内输入tomcat监听端口号, 推荐  %d %n", defaultTomcatPort, defaultTomcatPort);
+        strPort = scanner.nextLine();
+        tomcatPort = Convert.toInt(strPort);
+        scanner.close();
+
+        new SpringApplicationBuilder(MoocClassWebApplication.class)
+                .properties("spring.dubbo.protocol.port=" + dubboPort)
+                .properties("server.port=" + tomcatPort)
+                .run(args);
+
+
+//        SpringApplication.run(MoocClassWebApplication.class, args);
     }
 
     @Override
